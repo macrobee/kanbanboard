2fe0e461711bf517f3141df7dc8cec9fface2f4a
Vivian T <vivian.tong95@gmail.com>
2023-02-26 10:05:57 -0500

    moved header into a separate component for better code readability.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 
 import { ItemsListContext } from "./contexts/itemsListContext";
 
+import Header from './components/header';
 import Container from "./components/container";
 import Garbage from "./components/garbage";
 
@@ -10,14 +11,13 @@ function App() {
 
   return (
     <div className="h-[100vh] flex flex-col gap-2 justify-between items-center bg-gradient-to-b from-green-400 via-blue-500 to-purple-500 overflow-hidden font-mono">
-      <h1 className="text-xl font-bold self-start shadow-lg w-full bg-slate-200/50 px-4 py-2">
-        Kanban Board
-      </h1>
+      <Header text={"Kanban Board"} />
 
       <div className="grow w-auto flex p-2 gap-2 overflow-hidden">
         {Object.keys(currentItems).map((containerId) => (
           <Container id={containerId} key={containerId} />
         ))}
+
         <Garbage />
       </div>
     </div>
---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+
+const Header = ({text}) => {
+    return <h1 className="text-xl font-bold self-start shadow-lg w-full bg-slate-200/50 px-4 py-2">
+    {text}
+  </h1>
+}
+
+export default Header;
